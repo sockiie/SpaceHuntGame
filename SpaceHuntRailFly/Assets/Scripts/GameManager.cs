fbc52c4a73e2a5ad0952f3a93d91745d83436b54
sockiie <68150333+sockiie@users.noreply.github.com>
2021-02-11 11:18:22 +0100

    Game

---
+++ SpaceHuntRailFly/Assets/Scripts/GameManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Cinemachine;
+using System;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance = null;
+    public Text healthText;
+    public Text pointsText;
+    public PlayerMovement player;
+
+    public int playerHealth = 3;
+    public bool isGameOver = false;
+
+    public int points = 0;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this.gameObject);
+        }
+        else if (Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    void Update()
+    {
+        if (GameManager.Instance.isGameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                ResetGame();
+                UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+            }
+        }
+
+    }
+
+        internal void CoinPickUp()
+    {
+        points++;
+        pointsText.text = "Points: " + points;
+    }
+
+    public void PlayerHit()
+    {
+        playerHealth--;
+        healthText.text = "Health: " + playerHealth;
+        if (playerHealth == 0)
+        {
+            GameManager.Instance.GameOver();
+            player.LockMovement();
+          
+
+        }
+    }
+
+    public void ResetGame()
+    {
+        isGameOver = false;
+        
+        
+            points = 0;
+        
+       
+    }
+    public void GameOver()
+    {
+        isGameOver = true;
+    
+    }
+
+}
